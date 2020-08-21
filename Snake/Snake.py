262b9f33fe9fc45078a10979e0baf37f1f42ed83
BKSpiderman <patrickoboyle@Patricks-Air.fios-router.home>
2020-08-20 20:04:23 -0400

    commit

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -2,11 +2,10 @@ import pygame
 pygame.init()
 dis=pygame.display.set_mode((400,300))
 pygame.display.update()
-pygame.display.set_caption('Snake game by Edureka')
+pygame.display.set_caption('Snake game by Green Island Soft')
 game_over=False
 while not game_over:
     for event in pygame.event.get():
-        print(event)   #prints out all the actions that take place on the screen
-
+        print(event)
 pygame.quit()
 quit()
