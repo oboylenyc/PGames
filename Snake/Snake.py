6bf90f8fbafc8d2b57d702ee37d13b12462743cf
Patrick <13221589+oboylenyc@users.noreply.github.com>
2020-08-25 08:44:14 -0400

    Add files via upload

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -4,6 +4,7 @@ import random
 
 pygame.init()
 
+#colors
 white = (255, 255, 255)
 yellow = (255, 255, 102)
 black = (0, 0, 0)
@@ -11,38 +12,76 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
+#display
 dis_width = 800
 dis_height  = 800
 
 dis=pygame.display.set_mode((dis_width,dis_height))
 pygame.display.set_caption('Snake by Green Island Soft')
 
+#clockset
 clock = pygame.time.Clock()
 
+#snake graphics
 snake_block = 10
 snake_speed = 15
 
+#fonts
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
-
-def Your_score(score):
+#score display function
+def your_score(score):
     value = score_font.render("Your Score: " + str(score), True, yellow)
     dis.blit(value, [0, 0])
 
+#highscore display function
+def highscore_display(high_score):
+    value = score_font.render("High Score: " + str(high_score), True, yellow)
+    dis.blit(value, [0, 30])
 
+#get high schore from file function
+def get_high_score():
+    # Default high score
+    high_score = 0
+    # Try to read the high score from a file
+    try:
+        high_score_file = open("high_score.csv", "r")
+        high_score = int(high_score_file.read())
+        high_score_file.close()
+        #print("The high score is", high_score)
+    except IOError:
+        # Error reading file, no high score
+        print("There is no high score yet.")
+    except ValueError:
+        # There's a file there, but we don't understand the number.
+        print("I'm confused. Starting with no high score.")
 
+    return high_score
+
+#save high score to file function
+def save_high_score(new_high_score):
+    try:
+        # Write the file to disk
+        high_score_file = open("high_score.csv", "w")
+        high_score_file.write(str(new_high_score))
+        high_score_file.close()
+    except IOError:
+        # Hm, can't write it.
+        print("Unable to save the high score.")
+
+#snake function for display
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
 
-
+#display game over screen function
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
-
-def gameLoop():
+#main game function
+def main():
     game_over = False
     game_close = False
 
@@ -59,11 +98,15 @@ def gameLoop():
     foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
 
     while not game_over:
+        # Get the high score
+        high_score = get_high_score()
 
         while game_close == True:
             dis.fill(blue)
             message("Love you Dani! Press C-Play Again or Q-Quit", red)
-            Your_score(Length_of_snake - 1)
+            your_score(Length_of_snake - 1)
+            highscore_display(high_score)
+
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -72,7 +115,7 @@ def gameLoop():
                         game_over = True
                         game_close = False
                     if event.key == pygame.K_c:
-                        gameLoop()
+                        main()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -109,7 +152,23 @@ def gameLoop():
                 game_close = True
 
         our_snake(snake_block, snake_List)
-        Your_score(Length_of_snake - 1)
+        your_score(Length_of_snake - 1)
+        highscore_display(high_score)
+        # Get the score from the current game
+        current_score = 0
+        try:
+        # Ask the user for his/her score
+            current_score = int((Length_of_snake - 1))
+        except ValueError:
+        # Error, can't turn what they typed into a number
+            print("Score Error - Current Score")
+
+        if current_score > high_score:
+        # We do! Save to disk
+            print("Yea! New high score!")
+            save_high_score(current_score)
+        else:
+            pass
 
         pygame.display.update()
 
@@ -123,5 +182,4 @@ def gameLoop():
     pygame.quit()
     quit()
 
-
-gameLoop()
+main()
